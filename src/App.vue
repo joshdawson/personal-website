930e8a0406232ceaab003bf851bb868e07cfe342
Josh Dawson <joshtdawson11@gmail.com>
2024-02-19 10:16:12 +0000

    Add services page

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
     
     <p><a class="menu-link" href="#about">ABOUT</a></p>
     <p><a class="menu-link" href="#experience">EXPERIENCE</a></p>
-    <p><a class="menu-link" href="#education">EDUCATION</a></p>
+    <p><a class="menu-link" href="#services">SERVICES</a></p>
     <p><a class="menu-link" href="#skills">SKILLS</a></p>
   </aside>
 
@@ -18,10 +18,10 @@
       <section class="content" id="about">
         <h1>JOSH DAWSON</h1>
         <h4 class="reduced-margin">Freelance senior full stack developer / architect</h4>
-        <!-- <p><a href="mailto:joshdawson@progmaticsoftware.co.uk">Get in touch</a></p> -->
-        <!-- <h5 class="sub-heading">Whitley Bay, Tyne & Wear | <a href="mailto:joshdawson@progmaticsoftware.co.uk">joshdawson@progmaticsoftware.co.uk</a></h5> -->
+        <p><a href="mailto:joshdawson@progmaticsoftware.co.uk">joshdawson@progmaticsoftware.co.uk</a></p>
 
         <p>Dynamic senior full stack developer, architect, and mentor: 7+ years' expertise in Node.js, TypeScript, GraphQL, Angular & PostgreSQL. Proven leadership in project development & excellent communication. Passionate advocate for TDD & innovation, driving continuous improvement.</p>
+        
         <p>
           <a href="https://www.linkedin.com/in/josh-dawson-674815228/">
             <svg class="social-icon-wrapper no-margin" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
@@ -76,45 +76,67 @@
         </div>
       </section>
 
-      <section class="content" id="education">
-        <h1 class="reduced-margin">EDUCATION</h1>
+      <section class="content" id="services">
+        <h1 class="reduced-margin">SERVICES</h1>
 
-        <div class="experience-container">
-          <div class="experience-detail">
-            <h4>York St. John University</h4>
-
-            <p>First class honours in Computer Science (BSc Hons)<br />IEEE published author<br />Vice chancellor award for outstanding achievement</p>
-          </div>
-          <div class="experience-date">
-            <small class="duration">2013 - 2016</small>
-          </div>
+        <p>As a seasoned software developer and technical architect, I offer a range of services tailored to meet the diverse needs of clients in the digital realm. With over 7 years of hands-on experience and a proven track record of success, I specialize in the following areas:</p>
+      
+        <div class="services-heading">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="services-icon-container">
+            <path d="M24 10.935v2.131l-8 3.947v-2.23l5.64-2.783-5.64-2.79v-2.223l8 3.948zm-16 3.848l-5.64-2.783 5.64-2.79v-2.223l-8 3.948v2.131l8 3.947v-2.23zm7.047-10.783h-2.078l-4.011 16h2.073l4.016-16z"/>
+          </svg>
+          
+          <h4 class="reduced-margin">Web Development</h4>
         </div>
 
-        <div class="experience-container">
-          <div class="experience-detail">
-            <h4>Heaton Manor School</h4>
+        <p>Leveraging cutting-edge technologies such as Node.js, TypeScript, Angular, and GraphQL, I craft robust and scalable web applications tailored to your specific requirements. Whether you need a dynamic single-page application or a SaaS solution, I deliver bespoke solutions that drive user engagement and business growth.</p>
 
-            <p>A-levels in Maths, Physics and Computing</p>
-          </div>
-          <div class="experience-date">
-            <small class="duration">2006 - 2013</small>
-          </div>
+        <div class="services-heading">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="services-icon-container">
+            <path d="M23 10.826v2.349c-1.562 0-3 1.312-3 2.857 0 2.181 1.281 5.968-6 5.968v-2.002c4.917 0 3.966-1.6 3.966-3.967 0-2.094 1.211-3.5 2.278-4.031-1.067-.531-2.278-1.438-2.278-3.312 0-2.372.94-4.692-3.966-4.686v-2.002c7.285 0 6 4.506 6 6.688 0 1.544 1.438 2.138 3 2.138zm-19-2.138c0-2.182-1.285-6.688 6-6.688v2.002c-4.906-.007-3.966 2.313-3.966 4.686 0 1.875-1.211 2.781-2.278 3.312 1.067.531 2.278 1.938 2.278 4.031 0 2.367-.951 3.967 3.966 3.967v2.002c-7.281 0-6-3.787-6-5.969 0-1.545-1.438-2.857-3-2.857v-2.349c1.562.001 3-.593 3-2.137z"/>
+          </svg>
+          
+          <h4 class="reduced-margin">API Development</h4>
         </div>
+        
+        <p>Building upon my expertise in GraphQL, RESTful architecture, and API design principles, I develop secure and efficient APIs that facilitate seamless communication between disparate systems. From custom API integrations to comprehensive backend solutions, I ensure that your applications are equipped with robust and scalable APIs that power modern digital experiences.</p>
+      
+        <div class="services-heading">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="services-icon-container">
+            <path d="M5 18.184v-11.368c1.163-.413 2-1.511 2-2.816 0-1.657-1.343-3-3-3s-3 1.343-3 3c0 1.305.837 2.403 2 2.816v11.367c-1.163.414-2 1.512-2 2.817 0 1.657 1.343 3 3 3s3-1.343 3-3c0-1.305-.837-2.403-2-2.816zm-2.8-14.184c0-.993.807-1.8 1.8-1.8s1.8.807 1.8 1.8-.807 1.8-1.8 1.8-1.8-.807-1.8-1.8zm1.8 18.8c-.993 0-1.8-.807-1.8-1.8s.807-1.8 1.8-1.8 1.8.807 1.8 1.8-.807 1.8-1.8 1.8zm17-4.616v-11.184c0-2.206-1.794-4-4-4h-3v-3l-4 4 4 4v-3h3c1.103 0 2 .897 2 2v11.184c-1.163.413-2 1.512-2 2.816 0 1.657 1.343 3 3 3s3-1.343 3-3c0-1.305-.837-2.403-2-2.816zm-1 4.616c-.993 0-1.8-.807-1.8-1.8s.807-1.8 1.8-1.8 1.8.807 1.8 1.8-.807 1.8-1.8 1.8z"/>
+          </svg>
+
+          <h4 class="reduced-margin">Code Reviews</h4>
+        </div>
+ 
+        <p>Drawing upon my extensive experience in software engineering and technical leadership, I offer comprehensive code review services aimed at identifying potential vulnerabilities, improving code quality, and enhancing overall maintainability. Through meticulous analysis and constructive feedback, I help teams optimize their codebase, adhere to best practices, and mitigate technical debt, ensuring the long-term success of their projects.</p>
+
+        <div class="services-heading">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="services-icon-container">
+            <path d="M2.001 9.352c0 1.873.849 2.943 1.683 3.943.031 1 .085 1.668-.333 3.183 1.748-.558 2.038-.778 3.008-1.374 1 .244 1.474.381 2.611.491-.094.708-.081 1.275.055 2.023-.752-.06-1.528-.178-2.33-.374-1.397.857-4.481 1.725-6.649 2.115.811-1.595 1.708-3.785 1.661-5.312-1.09-1.305-1.705-2.984-1.705-4.695-.001-4.826 4.718-8.352 9.999-8.352 5.237 0 9.977 3.484 9.998 8.318-.644-.175-1.322-.277-2.021-.314-.229-3.34-3.713-6.004-7.977-6.004-4.411 0-8 2.85-8 6.352zm20.883 10.169c-.029 1.001.558 2.435 1.088 3.479-1.419-.258-3.438-.824-4.352-1.385-.772.188-1.514.274-2.213.274-3.865 0-6.498-2.643-6.498-5.442 0-3.174 3.11-5.467 6.546-5.467 3.457 0 6.546 2.309 6.546 5.467 0 1.12-.403 2.221-1.117 3.074zm-7.563-3.021c0-.453-.368-.82-.82-.82s-.82.367-.82.82.368.82.82.82.82-.367.82-.82zm3 0c0-.453-.368-.82-.82-.82s-.82.367-.82.82.368.82.82.82.82-.367.82-.82zm3 0c0-.453-.368-.82-.82-.82s-.82.367-.82.82.368.82.82.82.82-.367.82-.82z"/>
+          </svg>
+
+          <h4 class="reduced-margin">Mentoring</h4>
+        </div>
+
+        <p>With a passion for knowledge sharing and talent development, I provide personalized mentoring and coaching services to aspiring developers and seasoned professionals alike. Whether you're seeking guidance on specific technologies, project management methodologies, or career advancement strategies, I offer valuable insights, practical advice, and actionable feedback to help you achieve your goals.</p>
+
+        <p>Feel free to <a href="mailto:joshdawson@progmaticsoftware.co.uk">reach out</a> to discuss your specific project requirements or to explore how I can assist you in achieving your objectives.</p>
       </section>
 
       <section class="content" id="skills">
         <h1 class="reduced-margin">SKILLS</h1>
         <h4>Technical skills</h4>
         <h5 class="reduced-margin">Expert in</h5>
-        <p>JavaScript (Node.js), TypeScript, Jest, Jasmine, Angular, GraphQL</p>
+        <p>JavaScript, Node.js, TypeScript, Angular, GraphQL, Jest, Jasmine, TDD</p>
 
         <h5 class="reduced-margin">Experienced at</h5>
-        <p>HTML, CSS, React, AWS, Linux, PostgreSQL, Elasticsearch, OAuth, Vue.js, TDD</p>
+        <p>AWS, Nest.js, PostgreSQL, Elasticsearch, OAuth, Linux, HTML, CSS, React, Vue.js, Integration testing</p>
 
         <h5 class="reduced-margin">Exposure to</h5>
-        <p>C#, GCP, Docker, Terraform, Azure, Python, AI & LLMs</p>
+        <p>.NET, GCP, Docker, Terraform, Azure, Python, AI (LLMs, NLP), End-to-end testing, SPARQL</p>
 
-        <h4 class="reduced-margin">Other skills</h4>
+        <h4 class="reduced-margin">Practical skills</h4>
         <p>Team leadership, project management, mentoring, time management</p>
       </section>
     </div>
@@ -227,6 +249,16 @@
       fill: #18768C;
       
     }
+  }
+
+  .services-heading {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+
+  .services-icon-container {
+    margin-right: .5rem;
   }
 
   @media (max-width: 1000px) {
