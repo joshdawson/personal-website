32bab32413acd69973de0a47690de26fe7f9ed0d
Josh Dawson <joshtdawson11@gmail.com>
2025-10-27 15:19:48 +0000

    Add future find experience

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -1,17 +1,49 @@
 <script setup lang="ts">
-import Card from 'primevue/card';
+import Card from "primevue/card";
 </script>
 
 <template>
   <h1>EXPERIENCE</h1>
 
   <Card class="experience-card">
-    <template #title>Freelance Software Developer @ Valifi Ltd</template>
+    <template #title>Software Development Coach @ Skiller Whale</template>
+    <template #subtitle>April 2025 - Present</template>
+
+    <template #content>
+      <small>Freelance</small>
+      <p>
+        Delivering coaching sessions on advanced TypeScript topics including
+        generics, utility types, and function overloading.
+      </p>
+    </template>
+  </Card>
+
+  <Card class="experience-card">
+    <template #title>Backend Developer @ Future_find Ltd</template>
+    <template #subtitle>July 2025 - Present</template>
+
+    <template #content>
+      <small>Freelance</small>
+      <p>
+        Partnered with a founder in the recruitment industry to design and
+        deliver an AI-powered recruitment platform MVP. Defined the cloud
+        architecture using IaC and developed the full stack solution, including
+        REST APIs and a modern frontend application.
+      </p>
+    </template>
+  </Card>
+
+  <Card class="experience-card">
+    <template #title>Backend Developer @ Valifi Ltd</template>
     <template #subtitle>January 2025 - April 2025</template>
 
     <template #content>
       <small>Freelance</small>
-      <p>Collaborated with a UK FinTech startup on various ad-hoc tasks including expanding the automated testing suite to improve code reliability, and developing scheduled jobs to remove manual processes.</p>
+      <p>
+        Collaborated with a UK FinTech startup on various ad-hoc tasks including
+        expanding the automated testing suite to improve code reliability, and
+        developing scheduled jobs to remove manual processes.
+      </p>
     </template>
   </Card>
 
@@ -21,7 +53,11 @@ import Card from 'primevue/card';
 
     <template #content>
       <small>Contract</small>
-      <p>Architected and built a brand new solution for an AI tool for the creative industry. Provisioned infrastructure, built CI/CD pipelines, and led backend development.</p>
+      <p>
+        Architected and built a brand new solution for an AI tool for the
+        creative industry. Provisioned infrastructure, built CI/CD pipelines,
+        and led backend development.
+      </p>
     </template>
   </Card>
 
@@ -31,7 +67,10 @@ import Card from 'primevue/card';
 
     <template #content>
       <small>Part-time</small>
-      <p>Offering technical advice on a yet to be released AI-enhanced brand strategy tool.</p>
+      <p>
+        Offering technical advice on a yet to be released AI-enhanced brand
+        strategy tool.
+      </p>
     </template>
   </Card>
 
@@ -41,7 +80,11 @@ import Card from 'primevue/card';
 
     <template #content>
       <small>Full-time</small>
-      <p>Provided technical leadership for strategically important technical requirements through prototyping, de-risking, architectural designs and robust technical documentation.</p>
+      <p>
+        Provided technical leadership for strategically important technical
+        requirements through prototyping, de-risking, architectural designs and
+        robust technical documentation.
+      </p>
     </template>
   </Card>
 
@@ -51,7 +94,12 @@ import Card from 'primevue/card';
 
     <template #content>
       <small>Full-time</small>
-      <p>First software engineering hire at a fast paced MedTech start up. Introduced automated tests to untested codebases,  helped introduce software development process and contributed to implementation and architecture of new hospital integration system.</p>
+      <p>
+        First software engineering hire at a fast paced MedTech start up.
+        Introduced automated tests to untested codebases, helped introduce
+        software development process and contributed to implementation and
+        architecture of new hospital integration system.
+      </p>
     </template>
   </Card>
 
@@ -61,27 +109,39 @@ import Card from 'primevue/card';
 
     <template #content>
       <small>Full-time</small>
-      <p>Instrumental in helping build new SaaS offering from the ground up. Involved from proof of concept phase, leading development of multiple features within a scrum team alongisde mentoring junior members of the team.</p>
+      <p>
+        Instrumental in helping build new SaaS offering from the ground up.
+        Involved from proof of concept phase, leading development of multiple
+        features within a scrum team alongisde mentoring junior members of the
+        team.
+      </p>
     </template>
   </Card>
 
-  <p class="full-cv">For a comprehensive overview of my experience and skills <a class="cv-link" href="https://drive.google.com/file/d/1UJpp17BI0EvO6v5g5bxyIB6ll76jBzzE/view?usp=sharing">explore my full CV</a> </p>
+  <p class="full-cv">
+    For a comprehensive overview of my experience and skills
+    <a
+      class="cv-link"
+      href="https://drive.google.com/file/d/1UJpp17BI0EvO6v5g5bxyIB6ll76jBzzE/view?usp=sharing"
+      >explore my full CV</a
+    >
+  </p>
 </template>
 
 <style scoped>
-  .experience-card {
-    margin-top: 2rem;
-  }
+.experience-card {
+  margin-top: 2rem;
+}
 
-  .experience-title {
-    color: var(--theme-highlight);
-  }
+.experience-title {
+  color: var(--theme-highlight);
+}
 
-  .cv-link {
-    font-weight: 400;
-  }
+.cv-link {
+  font-weight: 400;
+}
 
-  .full-cv {
-    margin-top: 2rem;
-  }
+.full-cv {
+  margin-top: 2rem;
+}
 </style>
