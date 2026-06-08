22a1e0e91d390006571143c94bb26048cd7e82b1
Josh Dawson <joshtdawson11@gmail.com>
2026-06-08 15:25:13 +0100

    Update dependencies
    Update npm deps, github actions, upgrade to node 24

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,7 @@
 import { createApp } from 'vue';
-import PrimeVue from 'primevue/config';
 import './style.css';
-import 'primevue/resources/themes/aura-light-blue/theme.css';
-import 'primeicons/primeicons.css'
+import 'primeicons/primeicons.css';
 import App from './App.vue';
-import Button from 'primevue/button';
-import Divider from 'primevue/divider';
 
 const app = createApp(App);
-app.use(PrimeVue);
-
-app.component('Button', Button);
-app.component('Divider', Divider);
-
 app.mount('#app');
